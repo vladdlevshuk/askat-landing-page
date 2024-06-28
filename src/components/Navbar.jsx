ef19f6760911ca71513ff7e732be93fc6336a992
vladdlevshuk <vlad1levshuk@gmail.com>
2024-06-28 04:22:57 +0300

    fix: fix navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-// import logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
 // react icons
-import { FaXmark, FaBars, FaSquareWhatsapp, FaTelegram } from 'react-icons/fa6';
+import { FaXmark, FaBars, FaWhatsapp, FaTelegram } from 'react-icons/fa6';
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -49,11 +49,11 @@ const Navbar = () => {
 
     return (
         <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-            <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" : ""}`}>
+            <nav className={`py-5 lg:px-14 px-5 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" : ""}`}>
                 <div className='flex justify-between items-center text-base gap-8'>
-                    <a href="" className='text-3xl font-semibold flex items-center space-x-3'>
-                        {/* <img src={logo} alt="" className='w-10 inline-block items-center' /> */}
-                        <span className='text-[#263238]'>ASKAT</span>
+                    <a href="" className='text-4xl font-semibold flex items-center'>
+                        <img src={logo} alt="Logo" className='w-9 inline-block items-center' />
+                        <span className='text-[#263238] ml-0'>ASKAT</span>
                     </a>
 
                     {/* nav items for large devices */}
@@ -65,7 +65,7 @@ const Navbar = () => {
                                 smooth={true}
                                 offset={-100}
                                 key={path}
-                                className='block text-base text-gray900 hover:text-brandPrimary cursor-pointer first:font-medium'
+                                className='block text-lg text-gray900 hover:text-brandPrimary cursor-pointer first:font-medium'
                             >
                                 {link}
                             </Link>
@@ -73,25 +73,27 @@ const Navbar = () => {
                     </ul>
 
                     {/* icons and button for large devices */}
-                    <div className='space-x-4 hidden lg:flex items-center'>
-                        <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
-                            <FaSquareWhatsapp className='text-neutralDGrey h-6 w-6 hover:text-brandPrimary' />
-                        </a>
-                        <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer">
-                            <FaTelegram className='text-neutralDGrey h-6 w-6 hover:text-brandPrimary' />
-                        </a>
-                        <button className='bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey cursor-pointer'>
-                            Запросить предложение
+                    <div className='hidden lg:flex items-center gap-4'>
+                        <div className='hidden lg:flex justify-between align-center gap-4'>
+                            <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer">
+                                <FaTelegram className='text-neutralDGrey h-6 w-6 hover:text-brandPrimary' />
+                            </a>
+                            <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
+                                <FaWhatsapp  className='text-neutralDGrey h-6 w-6 hover:text-brandPrimary' />
+                            </a>
+                        </div>
+                        <button className='bg-brandPrimary text-lg text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey cursor-pointer'>
+                            Заказать
                         </button>
                     </div>
 
                     {/* menu btn for only mobile devices */}
                     <div className='flex items-center md:hidden'>
-                        <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
-                            <FaSquareWhatsapp className='text-neutralDGrey h-6 w-6 mr-2' />
-                        </a>
                         <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer">
-                            <FaTelegram className='text-neutralDGrey h-6 w-6 mr-2' />
+                                <FaTelegram className='text-neutralDGrey h-6 w-6 mr-4' />
+                        </a>
+                        <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
+                            <FaWhatsapp  className='text-neutralDGrey h-6 w-6 mr-4' />
                         </a>
                         <button
                             onClick={toggleMenu}
@@ -117,7 +119,7 @@ const Navbar = () => {
                         </Link>
                     ))}
                     <button className='block text-base text-white cursor-pointer first:font-medium'>
-                        Запросить предложение
+                        Заказать
                     </button>
                 </div>
             </nav>
