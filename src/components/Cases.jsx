1c64b79e907db9e19798e8c52250cb5d1feca89e
Vlad Levshuk <vlad1levshuk@gmail.com>
2024-09-08 15:26:01 +0300

    restyle: update cases component

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -1,53 +1,58 @@
-import React from 'react'
-import firstProject from '../assets/img/cases/first-project.png'
-import secondProject from '../assets/img/cases/second-project.png'
-import thirdProject from '../assets/img/cases/third-project.png'
+import React from 'react';
+import yandex from '../assets/img/cases/yandex.png';
+import osmicards from '../assets/img/cases/osmicards.png';
+import iiko from '../assets/img/cases/iiko.png';
 
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Cases = () => {
   const projects = [
-    {id: 1, title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', image: firstProject},
-    {id: 2, title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', image: secondProject},
-    {id: 3, title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', image: thirdProject},
+    { id: 1, title: 'Интеграция Яндекс.Еда с RetailCRM', image: yandex },
+    { id: 2, title: 'Интеграция Osmicards с RetailCRM', image: osmicards },
+    { id: 3, title: 'Интеграция IIKO с RetailCRM', image: iiko },
   ];
+
   return (
-    <div className='px-4 py-16 md:py-20 lg:px-14 max-w-screen 2xl mx-auto' style={{maxWidth: '1440px'}} id='cases'>
-        <motion.div
-          variants={fadeIn("", 0.1)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{ once: true, amount: 0.4 }}
-          className='text-center md:w-1/2 mx-auto'
-        >
-          <h2 className='mx-auto text-center lg:mx-0 text-3xl md:text-4xl text-neutralDGrey font-semibold mb-4'>Кейсы</h2>
-          <p className='text-lg text-neutralGrey mb-8 mx-auto'>Ознакомьтесь с нашими практическими примерами</p>
-        </motion.div>
-        {/* all projects */}
-        <div className='grid xl:grid-cols-3 grid-cols-1 gap-8 xl:gap-16 items-center justify-between'>
-          {
-            projects.map(project => <motion.div
-                variants={fadeIn("", 0.1)}
-                initial='hidden'
-                whileInView={'show'}
-                viewport={{ once: true, amount: 0.4 }}
-                key={project.id} className='mx-auto relative mb-12'
-              >
-              <img src={project.image} alt="Project Image" className='lg:hover:scale-95 cursor-pointer rounded-3xl transition-all duration-300 xl:max-w-96'/>
-              <div className='text-center px-4 py-8 bg-white shadow-lg rounded-3xl md:w-3/4 mx-auto absolute left-0 right-0 -bottom-12'>
-                <h3 className='mb-3 text-neutralGrey font-semibold'>{project.title}</h3>
-                <div className='flex items-center justify-center gap-8'>
-                  <a href="/" className='font-bold text-brandPrimary lg:hover:scale-105 lg:hover:text-neutralGrey transition-all duration-300'>
-                    Подробнее
-                  </a>
-                </div>
+    <div className='px-4 py-16 md:py-20 lg:px-14 max-w-screen-2xl mx-auto' style={{ maxWidth: '1440px' }} id='cases'>
+      <motion.div
+        variants={fadeIn("", 0.1)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: true, amount: 0.4 }}
+        className='text-center md:w-1/2 mx-auto'
+      >
+        <h2 className='mx-auto text-center lg:mx-0 text-3xl md:text-4xl text-neutralDGrey font-semibold mb-4'>Кейсы</h2>
+        <p className='text-lg text-neutralGrey mb-8 mx-auto'>с нашими практическими примерами:</p>
+      </motion.div>
+      <div className='grid xl:grid-cols-3 grid-cols-1 items-center justify-between'>
+        {projects.map(project => (
+          <motion.div
+            key={project.id}
+            variants={fadeIn("", 0.1)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: true, amount: 0.4 }}
+            className='mx-auto relative mb-12'
+          >
+            <div className='relative'>
+              <img
+                src={project.image}
+                alt="Project Image"
+                className='cursor-pointer rounded-3xl transition-all duration-300 max-w-96'
+              />
+              <div className='absolute inset-0 rounded-3xl flex flex-col items-center justify-center bg-gradient-to-b from-black/70 to-transparent'>
+                <h3 className='text-white text-xl max-w-72 font-semibold text-center px-4 mb-4'>{project.title}</h3>
+                <button className='text-white border border-white px-4 py-2 rounded-full hover:bg-white hover:text-black transition-all duration-300'>
+                  Подробнее
+                </button>
               </div>
-            </motion.div>)
-          }
-        </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cases
+export default Cases;
